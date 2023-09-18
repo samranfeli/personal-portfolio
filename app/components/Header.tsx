ac7edb9e081161c2eb51d5746f94f31228c0901f
Samran Feliimoghadam <s.feli@Itours.local>
2023-09-18 18:19:39 +0330

    try another background color for header dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,10 @@
 const Header = () => {
 
     return (
-        <header className="py-2 md:py-4 bg-black dark:bg-[#5f5f5f42]">
+        <header className="py-2 md:py-4 bg-black dark:bg-[#81818169]">
             <div className="max-w-7xl mx-auto px-3">
-                <div>
-                    <h1 className="text-lg md:text-3xl font-bold text-white">Samran Feli Moghadam</h1>
-                    <h2 className="md:text-lg font-semibold text-white">Front-end developer</h2>
-                </div>
+                <h1 className="text-lg md:text-3xl font-bold text-white">Samran Feli Moghadam</h1>
+                <h2 className="md:text-lg font-semibold text-white">Front-end developer</h2>
             </div>
         </header>
     )
