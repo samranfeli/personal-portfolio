7f2647bd1df6b8f4ec54fe4378de6968dc057a6a
Samran Feliimoghadam <s.feli@Itours.local>
2023-09-18 18:16:13 +0330

    add background color to header in dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
 
     return (
-        <header className="py-2 md:py-4 bg-black">
+        <header className="py-2 md:py-4 bg-black dark:bg-[#5f5f5f42]">
             <div className="max-w-7xl mx-auto px-3">
                 <div>
                     <h1 className="text-lg md:text-3xl font-bold text-white">Samran Feli Moghadam</h1>
